6f4f4e5dcf8621dfb6a54e200dc23823d8cab99a
Kacey Cleveland <kleveland@github.com>
2021-09-26 22:10:29 -0400

    initial gh-pages commit

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/kleveland/gif-creator.git', // Update to point to your repository  
+        user: {
+            name: 'Kacey Cleveland', // update to use your name
+            email: 'kaceycleveland.mail@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
